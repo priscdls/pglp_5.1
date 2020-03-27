4a68053ab40d39acf9d3013b8b423fefbcf65a79
Priscille Daoulas <priscilledaoulas@gmail.com>
2020-03-27 16:39:56 +0100

    Tests unitaires

---
+++ pglp_5_1/src/test/java/priscille/pglp_5_1/CompositePersonnelDaoTest.java
@@ -0,0 +1,14 @@
+package priscille.pglp_5_1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CompositePersonnelDaoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
